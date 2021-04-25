c1fd5bd53048169f6e1ef1359818f08d79593842
Armani020 <ar.amirgaliev.2015@gmail.com>
2021-04-26 02:15:02 +0600

    Adding a project

---
+++ Right_Location/Assets/Scripts/HouseDoor1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class HouseDoor1 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("_scene/BakerHouseInside");
+    }
+}
