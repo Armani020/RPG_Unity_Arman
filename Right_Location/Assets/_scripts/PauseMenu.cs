8c75e161dd676e4486396462a27fe6cf6589259a
Armani020 <ar.amirgaliev.2015@gmail.com>
2021-06-01 03:59:18 +0600

    - User Intarface updates
    - New main menu

---
+++ Right_Location/Assets/_scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("_scene/MainMenu2");
+    }
+}
